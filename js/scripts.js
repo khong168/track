d96fc91a665e280540b33ac527e0aa235d027d5d
hingyi khong <hingyi@gmail.com>
2018-08-10 09:58:36 -0700

    fixed syntax bug on line 25.

--- js/scripts.js
+++ js/scripts.js
@@ -11,23 +11,23 @@ $(document).ready(function() {
 
       if (developer == 'frontEnd' && company == 'small' && strength == 'design') {
         $("#frontEndResult").show();
-      // } else if (quality === 'kindness' && drunkFood === 'burritos') {
-      //   $("#fabulous").show();
-      // } else if (quality === 'honesty' && drunkFood === 'burritos') {
-      //   $("#brilliant").show();
-      // } else if (quality === 'bravery' && drunkFood === 'pizza') {
-      //   $('#brilliant').show();
-      // } else if (quality === 'kindness' && drunkFood === 'pizza') {
-      //   $('#positive').show();
-      // } else if (quality === 'honesty' && drunkFood === 'pizza') {
-      //   $('#cool').show();
-      // } else if (quality === 'bravery' && drunkFood === 'fries') {
-      //   $('#cool').show();
-      // } else if (quality === 'kindness' && drunkFood === 'fries') {
-      //   $('#adventurous').show();
-      // } else {
-      //   $('#brilliant').show();
-      // }
+      } else if (quality === 'kindness' && drunkFood === 'burritos') {
+        $("#fabulous").show();
+      } else if (quality === 'honesty' && drunkFood === 'burritos') {
+        $("#brilliant").show();
+      } else if (quality === 'bravery' && drunkFood === 'pizza') {
+        $('#brilliant').show();
+      } else if (quality === 'kindness' && drunkFood === 'pizza') {
+        $('#positive').show();
+      } else if (quality === 'honesty' && drunkFood === 'pizza') {
+        $('#cool').show();
+      } else if (quality === 'bravery' && drunkFood === 'fries') {
+        $('#cool').show();
+      } else if (quality === 'kindness' && drunkFood === 'fries') {
+        $('#adventurous').show();
+      } else {
+        $('#brilliant').show();
+      }
 
 
       event.preventDefault();
