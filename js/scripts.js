ab9f4c27a75d8af898bf0b8673dfe5a003a3e9a5
hingyi khong <hingyi@gmail.com>
2018-08-10 09:28:51 -0700

    create initial js variables.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $("form#surveyForm").submit(function(event) {
+      var developer = $("input[name='developer']:checked").val();
+      var company = $("input[name='company']:checked").val();
+
+      console.log(developer);
+      console.log(company);
+
+      if (quality === 'bravery' && drunkFood === 'burritos') {
+        $("#sexy").show();
+      } else if (quality === 'kindness' && drunkFood === 'burritos') {
+        $("#fabulous").show();
+      } else if (quality === 'honesty' && drunkFood === 'burritos') {
+        $("#brilliant").show();
+      } else if (quality === 'bravery' && drunkFood === 'pizza') {
+        $('#brilliant').show();
+      } else if (quality === 'kindness' && drunkFood === 'pizza') {
+        $('#positive').show();
+      } else if (quality === 'honesty' && drunkFood === 'pizza') {
+        $('#cool').show();
+      } else if (quality === 'bravery' && drunkFood === 'fries') {
+        $('#cool').show();
+      } else if (quality === 'kindness' && drunkFood === 'fries') {
+        $('#adventurous').show();
+      } else {
+        $('#brilliant').show();
+      }
+
+
+      event.preventDefault();
+    });
+  });
