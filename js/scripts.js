57362706a57ccb7ca5a368fcf514a97d3063657f
Student <example@example.com>
2018-08-16 13:02:50 -0700

    change variable names to be more descriptive.

--- js/scripts.js
+++ js/scripts.js
@@ -4,43 +4,43 @@ $(document).ready(function() {
         results.forEach(function(result) {
           $(result).hide();
         });
-        
-      var developer = $("input[name='developer']:checked").val();
-      var company = $("input[name='company']:checked").val();
+
+      var tmnt = $("input[name='tmnt']:checked").val();
+      var iceCream = $("input[name='iceCream']:checked").val();
       var strength = $("input[name='strength']:checked").val();
       var hobby = $("input[name='hobby']:checked").val();
       var beatles = $("input[name='beatles']:checked").val();
       var book = $("input[name='book']:checked").val();
 
-      // console.log(developer);
-      // console.log(company);
+      // console.log(tmnt);
+      // console.log(iceCream);
       // console.log(strength);
       // console.log(hobby);
       // console.log(beatles);
       // console.log(book);
 
 
-      if (developer === 'frontEnd' && company === 'small' && strength === 'design') {
+      if (tmnt === 'frontEnd' && iceCream === 'small' && strength === 'design') {
         $("#frontEndResult").show();
-      } else if (developer === 'frontEnd' && company === 'medium' && strength === 'logic') {
+      } else if (tmnt === 'frontEnd' && iceCream === 'medium' && strength === 'logic') {
         $("#fullStackResult").show();
-      } else if (developer === 'frontEnd' && company === 'large' && strength === 'logic') {
+      } else if (tmnt === 'frontEnd' && iceCream === 'large' && strength === 'logic') {
         $("#frontEndResult").show();
-      } else if (developer === 'backEnd' && company === 'large' && strength === 'patience') {
+      } else if (tmnt === 'backEnd' && iceCream === 'large' && strength === 'patience') {
         $("#backEndResult").show();
-      } else if (developer === 'backEnd' && company === 'large' && strength === 'logic') {
+      } else if (tmnt === 'backEnd' && iceCream === 'large' && strength === 'logic') {
         $("#backEndResult").show();
-      } else if (developer === 'backEnd' && company === 'medium' && strength === 'patience') {
+      } else if (tmnt === 'backEnd' && iceCream === 'medium' && strength === 'patience') {
         $('#fullStackResult').show();
-      } else if (developer === 'backEnd' && company === 'small' && strength === 'logic') {
+      } else if (tmnt === 'backEnd' && iceCream === 'small' && strength === 'logic') {
         $('#fullStackResult').show();
-      } else if (developer === 'undecided' && company === 'small' && strength === 'design') {
+      } else if (tmnt === 'undecided' && iceCream === 'small' && strength === 'design') {
         $('#frontEndResult').show();
-      } else if (developer === 'undecided' && company === 'large' && strength === 'logic') {
+      } else if (tmnt === 'undecided' && iceCream === 'large' && strength === 'logic') {
         $('#fullStackResult').show();
-      } else if (developer === 'undecided' && company === 'large' && strength === 'design') {
+      } else if (tmnt === 'undecided' && iceCream === 'large' && strength === 'design') {
         $('#frontEndResult').show();
-      } else if (developer === 'undecided' && company === 'medium' && strength === 'design') {
+      } else if (tmnt === 'undecided' && iceCream === 'medium' && strength === 'design') {
         $('#fullStackResult').show();
       } else if (book === '1984' && hobby === 'boardGames' && beatles === 'john') {
         $('#backEndResult').show();
